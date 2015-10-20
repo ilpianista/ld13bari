99dec08a661ca04c246f5e9dcbe56f929bf7acdf
Andrea Scarpino <me@andreascarpino.it>
2015-10-20 19:07:16 +0200

    Initial import

---
+++ slide_config.js
@@ -0,0 +1,33 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Shell Scripting<br/>for Daily Use',
+    //subtitle: 'subtitle',
+    eventInfo: {
+      title: 'Linux Day 2013 Bari',
+      date: '26/10/2013'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/favicon.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Monaco'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Andrea Scarpino',
+    company: 'Freelance Software Developer',
+    gplus: 'https://plus.google.com/+AndreaScarpino',
+    twitter: '@scarpin0',
+    www: 'http://www.andreascarpino.it/',
+    github: 'https://github.com/andreascarpino'
+  }]
+};
+
